d4a3e688e7b2b6db0cde11aed67c8931212210bc
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2022-12-09 05:33:53 -0500

    ajusteds mawuetacion

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -7,28 +7,67 @@
         span 4
       h1 Marco legal
 
-    .d-none.d-xl-block
-      br
-      br
-      br
-      br
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade-right")
+      .col-lg-7.col-12
+        h4.mb-4 El CONPES 3701, es la base de la legislación relacionada con esta temática. La Comisión Nacional Digital y de Información Estatal, fue creada a través del Decreto 32 de 2013 del Ministerio de Tecnologías de la Información y las Comunicaciones. 
+        p Petición que tiene el objeto 34 de encargarse de la coordinación y orientación superior del desarrollo de funciones y servicios públicos que tienen que ver con el manejo de la información pública, el uso de infraestructura tecnológica de la información, y la utilidad de la información en el Estado colombiano.
+        p.mb-lg-0.mb-3 Visite el recurso que le presentamos a continuación y conozca más y mejor sobre el marco legal en procesos de ciberseguridad y seguridad de la información. <b>Una vez más le insistimos en tomar nota de los aspectos más relevantes</b>.
+      .col-lg-5.col-8
+        img.mb-0(src="@/assets/curso/tema4/img01.png")
+    
+    TabsA.color-acento-botones(data-aos="fade-left")
+      .tarjeta.tarjeta1.p-lg-5.p-3( titulo="Proyectos")
+        h4 Proyectos
+        .row.justify-content-center
+          .col-lg-4.col-8
+            img.mb-lg-0.mb-3(src="@/assets/curso/tema4/img02.png")
+          .col-lg-8.col-12
+            ul.lista-ul--color.mb-lg-0.mb-3
+              li.mb-1
+                i.fas.fa-check(style="color:#2EFEC7")
+                p.mb-0 <b>Organizativo</b>: se refiere a la afección que se puede generar a la estructura de la organización el método de trabajo que utilicemos o similares.
+              li.mb-1
+                i.fas.fa-check(style="color:#2EFEC7")
+                p.mb-0 <b>Técnico</b>: se relaciona mucho con su nombre ya que son proyectos con un contenido técnico significativo. Por ejemplo, securizar la página web corporativa.
+              li.mb-1
+                i.fas.fa-check(style="color:#2EFEC7")
+                p.mb-0 <b>Regulatorio</b>: son proyectos enfocados a alinear algún aspecto concreto de la organización a alguna norma o regulación. Por ejemplo, los proyectos encaminados al cumplimiento de la LOPD y el RDLOPD.
+      .tarjeta.tarjeta1.p-lg-5.p-3( titulo="Asesorías ")
+        h4 Asesorías 
+        .row.justify-content-center
+          .col-lg-4.col-8
+            img.mb-lg-0.mb-3(src="@/assets/curso/tema4/img03.png")
+          .col-lg-8.col-12
+            p.mb-0 Se llevan a cabo después de un diagnóstico previo y su finalidad es brindar apoyo técnico en la toma de decisiones, relacionadas con las vulnerabilidades existentes, en materia de seguridad.
+      .tarjeta.tarjeta1.p-lg-5.p-3( titulo="Servicios outsourcing")
+        h4 Servicios outsourcing
+        .row.justify-content-center
+          .col-lg-4.col-8
+            img.mb-lg-0.mb-3(src="@/assets/curso/tema4/img04.png")
+          .col-lg-8.col-12
+            p.mb-0 Para una organización es importante contar con un agente especializado en seguridad, que proteja y prevenga los ataques informáticos. 
+      .tarjeta.tarjeta1.p-lg-5.p-3( titulo="Contratación")
+        h4 Contratación
+        .row.justify-content-center
+          .col-lg-4.col-8
+            img.mb-lg-0.mb-3(src="@/assets/curso/tema4/img05.png")
+          .col-lg-8.col-12
+            p.mb-0 Regirse por la legislación favorece que en los contratos independientes, se asegure la información con cláusulas de confidencialidad y no divulgación.
+      .tarjeta.tarjeta1.p-lg-5.p-3( titulo="Freelance")
+        h4 Freelance
+        .row.justify-content-center
+          .col-lg-4.col-8
+            img.mb-lg-0.mb-3(src="@/assets/curso/tema4/img06.png")
+          .col-lg-8.col-12
+            p.mb-0 Es la persona que trabaja de forma independiente, ofreciendo sus servicios a empresas u otras personas, y quien maneja su tiempo y su forma de trabajar de manera autónoma. En ciberseguridad un Freelance puede ayudar a complementar la seguridad digital teniendo en cuenta, entre otras, hacer copias de seguridad, trabajar con antivirus, usar escritorio virtual, proteger conexión a internet, reforzar seguridad de la web.
+      .tarjeta.tarjeta1.p-lg-5.p-3( titulo="Acuerdos de confidencialidad")
+        h4 Acuerdos de confidencialidad
+        .row.justify-content-center
+          .col-lg-4.col-8
+            img.mb-lg-0.mb-3(src="@/assets/curso/tema4/img07.png")
+          .col-lg-8.col-12
+            p.mb-02 Contrato de confidencialidad: cuando es necesario compartir la información de la organización, habrá que establecer las medidas de protección aplicables, por ejemplo: realizar una clasificación o utilizar técnicas de cifrado. 
       
-    //- .d-block.d-lg-none(data-aos="fade-right")
-    //-   .row.justify-content-center.mb-3
-    //-     .col-8
-    //-       img.mb-0(src='@/assets/curso/tema4/img01.png')      
-    .p-0.position-relative.px-2(data-aos="fade-right")
-      //- .row.justify-content-center
-      //-   .col-lg-2.d-flex
-      //-   .col-lg-3.d-flex(style="background-color:#ffeeeb")
-      //-   .col-lg-7.col-12.d-flex.py-5(style="background-color:#ffeeeb")
-      //-     p.mb-0 Para prevenir y combatir la delincuencia informática, <b>se ha avanzado a nivel global en la aplicación de campañas, técnicas, programas, normas y leyes que responden</b>, en cada país o región, a los eventos más concurrentes relativos a ese fenómeno.
-
-      .row.position-absolute.w-100.d-none.d-lg-block(style="bottom: 0px")
-        .col-lg-7.border.d-lg-block
-        .col-lg-3.border.d-lg-block
-          img.mb-0(src='@/assets/curso/tema4/img01.png')   
-        .col-lg-2.border.d-lg-block
 </template>
 
 <script>
--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -6,6 +6,45 @@
       .titulo-principal__numero
         span 6
       h1 Tipificación de delitos informáticos en Colombia
+
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade-right")
+      .col-lg-10.col-12
+        .bloque-texto-g.color-acento-contenido.p-lg-5.p-2
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/tema6/img01.png')})`}"
+          )
+          .bloque-texto-g__texto.p-lg-4.py-lg-5.p-2
+            p.mb-0 En la actualidad, el amplio uso de las tecnologías y la cantidad en aumento de usuarios en las redes y los sistemas ha favorecido la implantación de los delitos informáticos, es decir, todas aquellas acciones gravemente engañosas y de fraude que pueden cometerse en los ámbitos digitales y on line.
+
+    .fondo1.py-lg-5.py-3
+      .row.justify-content-center.align-items-center.mb-0.position-relative(data-aos="fade-left")
+        .col-lg-2.col-8
+          img.mb-lg-0.mb-3(src="@/assets/curso/tema6/img02.png")
+        .col-lg-8.col-12
+          h4.mb-4 Según la Ley 1273 de 2009, se ajustaron los delitos informáticos en Colombia en los siguientes términos: 
+          ul.lista-ul--color.mb-lg-0.mb-3
+            li.mb-2
+              i.fas.fa-check(style="color:#083142")
+              p.mb-0 Acceso abusivo a un sistema informático (modificado del Código Penal).
+            li.mb-2
+              i.fas.fa-check(style="color:#083142")
+              p.mb-0 Obstaculización ilegítima del sistema informático o red de telecomunicación.
+            li.mb-2
+              i.fas.fa-check(style="color:#083142")
+              p.mb-0 Interceptación de datos informáticos.
+            li.mb-2
+              i.fas.fa-check(style="color:#083142")
+              p.mb-0 Daño informático; uso de software malicioso.
+            li.mb-2
+              i.fas.fa-check(style="color:#083142")
+              p.mb-0 Hurto por medios informáticos y semejantes. 
+            li.mb-2
+              i.fas.fa-check(style="color:#083142")
+              p.mb-0 Violación de datos personales.
+            li.mb-2
+              i.fas.fa-check(style="color:#083142")
+              p.mb-0 Suplantación de sitios web para capturar datos personales y transferencia no consentida de activos.
+            
 </template>
 
 <script>
